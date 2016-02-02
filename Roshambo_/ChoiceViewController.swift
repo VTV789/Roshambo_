9489981ea4a658077c4d72587f85383cf2255078
Vinh Thanh Vu <vtv@vinh.com>
2016-02-01 23:12:52 -0500

    ResultsVC connect outlets for resultImage and resultLabel

---
+++ Roshambo_/ChoiceViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Roshambo_
+//
+//  Created by Vinh Vu on 1/28/16.
+//  Copyright © 2016 Vinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func playRock(sender: UIButton) {
+    }
+
+}
+
---
+++ Roshambo_/ResultsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.swift
+//  Roshambo_
+//
+//  Created by Vinh Vu on 1/30/16.
+//  Copyright © 2016 Vinh. All rights reserved.
+//
+
+import UIKit
+
+// The enum "Shape" represents a play or move 
+enum Shape: String {
+    case Rock = "Rock"
+    case Paper = "Pager"
+    case Scissors = "Scissors"
+    
+    // This func randomly generates an opponent's play
+    static func randomShape() -> Shape {
+        let shapes = ["Rock", "Paper", "Scissor"]
+        let randomChoice = Int(arc4random_uniform, (3))
+        return Shape(rawValue: shapes[randomChoice])!
+    }
+    
+}
+
+class ResultsViewController: UIViewController {
+    // MARK: Outlets
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: Shapes
+    // When the ResultsViewController is initialized a userChoice is passed and an opponent's play is generated.
+    var userChoice: Shape!
+    private let opponentChoice: Shape = Shape.randomShape()
+
+    
+}
