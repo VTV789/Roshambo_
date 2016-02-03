773f94b5870145cf765b8d202d8fdee047741ff0
Vinh Thanh Vu <vtv@vinh.com>
2016-02-02 22:02:11 -0500

    Renaming ViewController to ChoiceViewController (in 4 places)

--- Roshambo_/ChoiceViewController.swift
+++ Roshambo_/ChoiceViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChoiceViewController.swift
 //  Roshambo_
 //
 //  Created by Vinh Vu on 1/28/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChoiceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
